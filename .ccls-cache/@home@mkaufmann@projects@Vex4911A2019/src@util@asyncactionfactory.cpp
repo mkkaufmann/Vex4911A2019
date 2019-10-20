e8a49981b691ce9cf4261be986db2e6902b41c8b
mkkaufmann <mkaufmann421@gmail.com>
2019-10-20 15:31:05 -0400

    .gitignore fix

--- .ccls-cache/@home@mkaufmann@projects@Vex4911A2019/src@util@asyncactionfactory.cpp
+++
@@ -1,9 +0,0 @@
-#include "asyncactionfactory.hpp"
-#include "asyncaction.hpp"
-
-AsyncAction* AsyncActionFactory::makeAsyncAction(){
-  AsyncAction* action;
-  action = new AsyncAction;
-  *action = AsyncAction();
-  return action;
-}
